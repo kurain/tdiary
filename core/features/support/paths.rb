bff873bc501a25190567a096ae22270ce9af40b5
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-10-26 18:05:53 +0900

    add scenario: access a diary not exists

--- core/features/support/paths.rb
+++ core/features/support/paths.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module NavigationHelpers
 	# Maps a name to a path. Used by the
 	#
@@ -14,15 +15,17 @@ module NavigationHelpers
 					 month = "%02d" % $2
 					 day = "%02d" % $3
 					 "index.rb?date=#{year}#{month}#{day}"
-				when "日記の更新"
-					"update.rb"
-				when "基本設定"
-					"update.rb?conf=default"
-				# 必要があればここに追加する
-				else
-					raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-						"Now, go and add a mapping in #{__FILE__}"
-				end
+				 when "日記の更新"
+					 "update.rb"
+				 when "基本設定"
+					 "update.rb?conf=default"
+				 when "日記が存在しない日付"
+					 "index.rb?date=20380101"
+					 # 必要があればここに追加する
+				 else
+					 raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+						 "Now, go and add a mapping in #{__FILE__}"
+				 end
 		"#{TDiaryWorld::URL_BASE}/#{path}"
 	end
 end
