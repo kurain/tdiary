6235dfae1efb9e63519a5814b66a7f4356c0460b
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-10-26 13:53:13 +0900

    tabify

--- core/tdiary/response.rb
+++ core/tdiary/response.rb
@@ -2,22 +2,22 @@
 
 # taken from hiki/response.rb(rack branch in hiki), originated by okkez.
 module TDiary
-  if Object.const_defined?(:Rack)
-    Response = ::Rack::Response
-  else
-    class Response
-      attr_reader :body, :status, :headers
-      def initialize(body = [], status = 200, headers = {}, &block)
-        @cgi = CGI.new
-        @body = body
-        @status = status
-        @headers = headers
-        yield self if block_given?
-      end
+	if Object.const_defined?(:Rack)
+	 Response = ::Rack::Response
+	else
+		class Response
+			attr_reader :body, :status, :headers
+			def initialize(body = [], status = 200, headers = {}, &block)
+				@cgi = CGI.new
+				@body = body
+				@status = status
+				@headers = headers
+				yield self if block_given?
+			end
 
-      def header
-        @cgi.header(@headers)
-      end
-    end
-  end
+			def header
+				@cgi.header(@headers)
+			end
+		end
+	end
 end
